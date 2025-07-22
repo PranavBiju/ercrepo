031ea8ad87f50d0f762c70ee5f0469d624a21716
Pranav Biju <pranav.biju01@gmail.com>
2025-07-22 18:29:28 +0530

    done

--- anomaly_deduplicator/setup.py
+++ anomaly_deduplicator/setup.py
@@ -20,7 +20,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'anomaly_deduplicator = anomaly_deduplicator.anomaly_deduplicator_node:main'
+           
+           'deduplicator_node = anomaly_deduplicator.anomaly_deduplicator_node:main',
         ],
     },
 )
